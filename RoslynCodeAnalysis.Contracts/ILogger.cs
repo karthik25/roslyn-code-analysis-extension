8b1ae69d7839885de1d8a5abb1e6c420ade219e7
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-09-09 22:58:59 -0400

    added contracts proj for extending the functionality & a logger

---
+++ RoslynCodeAnalysis.Contracts/ILogger.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RoslynCodeAnalysis.Contracts
+{
+    public interface ILogger
+    {
+        void WriteLog(string message, params object[] args);
+        void WriteLog(Exception exception);
+    }
+}
---
+++ RoslynCodeAnalysis.SimpleLogger/TextLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using RoslynCodeAnalysis.Contracts;
+
+namespace RoslynCodeAnalysis.SimpleLogger
+{
+    [Export(typeof(ILogger))]
+    public class TextLogger : ILogger
+    {
+        private static readonly string LogPath = string.Format(@"{0}\RoslynLog.txt",
+                                                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+        public void WriteLog(string message, params object[] args)
+        {
+            WriteToFile(string.Format(message, args));
+        }
+
+        public void WriteLog(Exception exception)
+        {
+            WriteToFile(exception.ToString());
+        }
+
+        private static void WriteToFile(string data)
+        {
+            var logStream = new StreamWriter(LogPath, true);
+            logStream.WriteLine(data);
+            logStream.Close();
+        }
+    }
+}
