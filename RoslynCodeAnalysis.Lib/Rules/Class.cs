311696b1439e360b376eddc0ae6692f3ed4ea580
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-09-16 00:00:27 -0400

    rule classes for various constants

---
+++ RoslynCodeAnalysis.Lib/Rules/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace RoslynCodeAnalysis.Lib.Rules
+{
+    public static class Class
+    {
+        public static int IdealMethodCount = 10;
+        public static int IdealLineCount = 100;
+        public static int IdealNestingLevel = 1;
+    }
+}
---
+++ RoslynCodeAnalysis.Lib/Rules/Method.cs
@@ -0,0 +1,8 @@
+﻿namespace RoslynCodeAnalysis.Lib.Rules
+{
+    public static class Method
+    {
+        public static int IdealLineCount = 10;
+        public static string IdealNamePattern = "/[A-Z]{1}[a-zA-Z]+{3,}/";
+    }
+}
